24479db3693fc22c2da6bef801e49f3f0443bbb2
frreri <fredrik-eriksson@outlook.com>
2025-04-07 14:43:39 +0200

    separated out background functionality

--- script/background.js
+++ script/background.js
@@ -4,15 +4,13 @@ const randomBgBtn = document.getElementById('bg-btn');
 
 export const setupBackground = () => {
   const randomBg = () => {
-    console.log('test');
-    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)) , url('https://picsum.photos/${Math.floor(
-      window.innerWidth
-    )}/${Math.floor(window.innerHeight)}?random=${Math.floor(
-      Math.random() * 10000
-    )}')`;
+    const width = Math.floor(window.innerWidth);
+    const height = Math.floor(window.innerHeight);
+    const randNum = Math.floor(Math.random() * 10000);
+    const imgUrl = `https://picsum.photos/${width}/${height}?random=${randNum}`;
+    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) , url(${imgUrl})`;
   };
 
   randomBg();
-
   randomBgBtn.addEventListener('click', randomBg);
 };
--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 import { setupClock } from './clock.js';
-import { editHeading, editNotes } from './edit.js';
 import { setupLinks } from './links.js';
 import { setupWeather } from './weather.js';
 import { setupBackground } from './background.js';
+import { editHeading, editNotes } from './edit.js';
 
 const time = document.getElementById('time');
 const date = document.getElementById('date');
