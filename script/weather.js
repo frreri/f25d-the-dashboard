8f0063635cea76c8d014ef85869d9a8ee4ccc178
frreri <fredrik-eriksson@outlook.com>
2025-04-07 13:01:41 +0200

    uppdaterat api url

--- script/weather.js
+++ script/weather.js
@@ -3,7 +3,7 @@
 // weatherIcons is a map with weather code string as key
 import { weatherIcons } from '../data/weatherIcons.js';
 
-// API URL https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max&timezone=GMT&forecast_days=3
+// API URL https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max&timezone=Europe%2FBerlin&forecast_days=3
 
 export const setupWeather = () => {
   console.log(weatherIcons.get('0'));
