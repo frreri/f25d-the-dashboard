190fd17e582fa876e726b33128d773733e9e235b
frreri <fredrik-eriksson@outlook.com>
2025-04-07 12:55:08 +0200

    started working on weather

--- script/main.js
+++ script/main.js
@@ -4,6 +4,7 @@ import { setupClock } from './clock.js';
 import { setupNotes } from './notes.js';
 import { makeEditable } from './edit.js';
 import { setupLinks } from './links.js';
+import { setupWeather } from './weather.js';
 
 const time = document.getElementById('time');
 const date = document.getElementById('date');
@@ -12,5 +13,7 @@ const heading = document.getElementById('heading');
 
 setupClock(time, date);
 setupNotes(notes);
+setupWeather();
 setupLinks();
+
 makeEditable('dashH1', heading);
---
+++ script/weather.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// weatherIcons is a map with weather code string as key
+import { weatherIcons } from '../data/weatherIcons.js';
+
+// API URL https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max&timezone=GMT&forecast_days=3
+
+export const setupWeather = () => {
+  console.log(weatherIcons.get('0'));
+};
