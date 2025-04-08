d2547e5fd81a0aed616580348a29709e35a82525
frreri <fredrik-eriksson@outlook.com>
2025-04-08 11:00:18 +0200

    changed to https on icon urls

--- data/weatherIcons.js
+++ data/weatherIcons.js
@@ -2,115 +2,115 @@ export const weatherIcons = new Map(
   Object.entries({
     0: {
       description: 'Soligt',
-      image: 'http://openweathermap.org/img/wn/01d@2x.png',
+      image: 'https://openweathermap.org/img/wn/01d@2x.png',
     },
     1: {
       description: 'Mestadels Soligt',
-      image: 'http://openweathermap.org/img/wn/01d@2x.png',
+      image: 'https://openweathermap.org/img/wn/01d@2x.png',
     },
     2: {
       description: 'Delvis Molnigt',
-      image: 'http://openweathermap.org/img/wn/02d@2x.png',
+      image: 'https://openweathermap.org/img/wn/02d@2x.png',
     },
     3: {
       description: 'Molnigt',
-      image: 'http://openweathermap.org/img/wn/03d@2x.png',
+      image: 'https://openweathermap.org/img/wn/03d@2x.png',
     },
     45: {
       description: 'Dimma',
-      image: 'http://openweathermap.org/img/wn/50d@2x.png',
+      image: 'https://openweathermap.org/img/wn/50d@2x.png',
     },
     48: {
       description: 'Dimma',
-      image: 'http://openweathermap.org/img/wn/50d@2x.png',
+      image: 'https://openweathermap.org/img/wn/50d@2x.png',
     },
     51: {
       description: 'Lätt Duggregn',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     53: {
       description: 'Duggregn',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     55: {
       description: 'Kraftigt Duggregn',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     56: {
       description: 'Lätt Frysande Duggregn',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     57: {
       description: 'Frysande Duggregn',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     61: {
       description: 'Lätt Regn',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png',
+      image: 'https://openweathermap.org/img/wn/10d@2x.png',
     },
     63: {
       description: 'Regn',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png',
+      image: 'https://openweathermap.org/img/wn/10d@2x.png',
     },
     65: {
       description: 'Kraftigt Regn',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png',
+      image: 'https://openweathermap.org/img/wn/10d@2x.png',
     },
     66: {
       description: 'Lätt Frysande Regn',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png',
+      image: 'https://openweathermap.org/img/wn/10d@2x.png',
     },
     67: {
       description: 'Frysande Regn',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png',
+      image: 'https://openweathermap.org/img/wn/10d@2x.png',
     },
     71: {
       description: 'Lätt Snöande',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     73: {
       description: 'Snöande',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     75: {
       description: 'Kraftig Snöande',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     77: {
       description: 'Snökorn',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     80: {
       description: 'Lätta Skurar',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     81: {
       description: 'Skurar',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     82: {
       description: 'Kraftiga Skurar',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png',
+      image: 'https://openweathermap.org/img/wn/09d@2x.png',
     },
     85: {
       description: 'Lätta Snöskurar',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     86: {
       description: 'Snöskurar',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png',
+      image: 'https://openweathermap.org/img/wn/13d@2x.png',
     },
     95: {
       description: 'Åska',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png',
+      image: 'https://openweathermap.org/img/wn/11d@2x.png',
     },
     96: {
       description: 'Lätt Åska med Hagel',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png',
+      image: 'https://openweathermap.org/img/wn/11d@2x.png',
     },
     99: {
       description: 'Åska med Hagel',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png',
+      image: 'https://openweathermap.org/img/wn/11d@2x.png',
     },
   })
 );
