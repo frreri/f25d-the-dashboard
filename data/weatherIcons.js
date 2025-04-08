32a306feb07d43d2d21c742395f8a8905f6318f7
frreri <fredrik-eriksson@outlook.com>
2025-04-08 09:37:41 +0200

    översatt desc

--- data/weatherIcons.js
+++ data/weatherIcons.js
@@ -1,115 +1,115 @@
 export const weatherIcons = new Map(
   Object.entries({
     0: {
-      description: 'Sunny',
+      description: 'Soligt',
       image: 'http://openweathermap.org/img/wn/01d@2x.png',
     },
     1: {
-      description: 'Mainly Sunny',
+      description: 'Mestadels Soligt',
       image: 'http://openweathermap.org/img/wn/01d@2x.png',
     },
     2: {
-      description: 'Partly Cloudy',
+      description: 'Delvis Molnigt',
       image: 'http://openweathermap.org/img/wn/02d@2x.png',
     },
     3: {
-      description: 'Cloudy',
+      description: 'Molnigt',
       image: 'http://openweathermap.org/img/wn/03d@2x.png',
     },
     45: {
-      description: 'Foggy',
+      description: 'Dimma',
       image: 'http://openweathermap.org/img/wn/50d@2x.png',
     },
     48: {
-      description: 'Rime Fog',
+      description: 'Dimma',
       image: 'http://openweathermap.org/img/wn/50d@2x.png',
     },
     51: {
-      description: 'Light Drizzle',
+      description: 'Lätt Duggregn',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     53: {
-      description: 'Drizzle',
+      description: 'Duggregn',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     55: {
-      description: 'Heavy Drizzle',
+      description: 'Kraftigt Duggregn',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     56: {
-      description: 'Light Freezing Drizzle',
+      description: 'Lätt Frysande Duggregn',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     57: {
-      description: 'Freezing Drizzle',
+      description: 'Frysande Duggregn',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     61: {
-      description: 'Light Rain',
+      description: 'Lätt Regn',
       image: 'http://openweathermap.org/img/wn/10d@2x.png',
     },
     63: {
-      description: 'Rain',
+      description: 'Regn',
       image: 'http://openweathermap.org/img/wn/10d@2x.png',
     },
     65: {
-      description: 'Heavy Rain',
+      description: 'Kraftigt Regn',
       image: 'http://openweathermap.org/img/wn/10d@2x.png',
     },
     66: {
-      description: 'Light Freezing Rain',
+      description: 'Lätt Frysande Regn',
       image: 'http://openweathermap.org/img/wn/10d@2x.png',
     },
     67: {
-      description: 'Freezing Rain',
+      description: 'Frysande Regn',
       image: 'http://openweathermap.org/img/wn/10d@2x.png',
     },
     71: {
-      description: 'Light Snow',
+      description: 'Lätt Snöande',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     73: {
-      description: 'Snow',
+      description: 'Snöande',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     75: {
-      description: 'Heavy Snow',
+      description: 'Kraftig Snöande',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     77: {
-      description: 'Snow Grains',
+      description: 'Snökorn',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     80: {
-      description: 'Light Showers',
+      description: 'Lätta Skurar',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     81: {
-      description: 'Showers',
+      description: 'Skurar',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     82: {
-      description: 'Heavy Showers',
+      description: 'Kraftiga Skurar',
       image: 'http://openweathermap.org/img/wn/09d@2x.png',
     },
     85: {
-      description: 'Light Snow Showers',
+      description: 'Lätta Snöskurar',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     86: {
-      description: 'Snow Showers',
+      description: 'Snöskurar',
       image: 'http://openweathermap.org/img/wn/13d@2x.png',
     },
     95: {
-      description: 'Thunderstorm',
+      description: 'Åska',
       image: 'http://openweathermap.org/img/wn/11d@2x.png',
     },
     96: {
-      description: 'Light Thunderstorms With Hail',
+      description: 'Lätt Åska med Hagel',
       image: 'http://openweathermap.org/img/wn/11d@2x.png',
     },
     99: {
-      description: 'Thunderstorm With Hail',
+      description: 'Åska med Hagel',
       image: 'http://openweathermap.org/img/wn/11d@2x.png',
     },
   })
